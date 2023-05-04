1d9a4c1b5cbb15de388f0dab72bb5c6b446999cd
Ran Nahmany <ran162534@gmail.com>
2023-05-04 10:29:12 +0300

    Added Partial Support on Windows 11 and update py2exe configs

--- README.md
+++ README.md
@@ -3,14 +3,13 @@
 <!-- # LidLocker
 <img src="./images/OFF-tray.png" alt="LidLocker" width=40/> -->
 
-### The perfect app to work on your desktop monitor with your laptop lid closed.
+### The perfect app to work on your desktop monitor with your windows 10/11 laptop lid closed.
 
-Tiny tray app that lets you close your laptop and still work on the Monitor.
+Tiny tray app that lets you close your windows 10/11 laptop without going to sleep and still work on the Monitor.
 Simply control what happens when you close your laptop lid. Go to sleep or do nothing to work on your desktop monitor.
 
 
 ## States
-
 ### The app has two simple states:
 
 1. <u> OFF (Red Icon) -</u> when you close the lid, your laptop will go to sleep as the default behavior.
@@ -25,7 +24,10 @@ https://github.com/RanNahmany/LidLocker/releases
 
 <br>
 
-## Used packages:
-* [pystray](https://github.com/moses-palmer/pystray)
-* [pillow](https://github.com/python-pillow/Pillow)
-* [auto-py-to-exe](https://github.com/brentvollebregt/auto-py-to-exe)
+## Development:
+
+```Python
+pip install -r requirements.txt
+```
+
+
--- main.py
+++ main.py
@@ -4,6 +4,30 @@ import pystray
 import PIL.Image
 import subprocess
 
+def execute_command(command):
+    try:
+        CREATE_NO_WINDOW = 0x08000000
+        output = subprocess.check_output(command, creationflags=CREATE_NO_WINDOW).decode('utf-8', errors='ignore')
+        return output
+    
+    except Exception as e:
+        print (e)
+        return None
+
+# determine if the OS is Windows 10 or 11 and return '10' or '11' accordingly
+def __is_win10_or_11():
+    output = execute_command('systeminfo')
+    output = output.split('\n')
+
+    for line in output:
+        if 'OS Name:' in line:
+            if 'Windows 10' in line:
+                return '10'
+            elif 'Windows 11' in line:
+                return '11'
+            else:
+                return None
+
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -16,60 +40,61 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def execute_command(command):
-    try:
-        CREATE_NO_WINDOW = 0x08000000
-        output = subprocess.check_output(command, creationflags=CREATE_NO_WINDOW).decode('utf-8', errors='ignore')
-        return output
-    
-    except Exception as e:
-        print (e)
-        return None
-
 def __hex_to_dec(hex_num):
     return int(hex_num, 16)
 
+
 def __is_State_ON():
-    output = execute_command('powercfg /q')
-    output = output.split('\n')
 
-    index = 0
-    for line in output:
-        if 'Subgroup GUID: 4f971e89-eebd-4455-a8de-9e59040e7347' in line and 'Power Setting GUID: 5ca83367-6e45-459f-a27b-476b1d01c936' in output[index + 2]:
-            AC = output[index + 12]
-            DC = output[index + 13]
+    if OS_VERSION == '10':
+        output = execute_command('powercfg /q')
+        output = output.split('\n')
 
-            if 'Current AC Power Setting Index:' in AC and 'Current DC Power Setting Index:' in DC:
-                hex_AC = AC.strip().split(':')[1].strip()
-                dec_AC = __hex_to_dec(hex_AC)
+        index = 0
+        for line in output:
+            if 'Subgroup GUID: 4f971e89-eebd-4455-a8de-9e59040e7347' in line and 'Power Setting GUID: 5ca83367-6e45-459f-a27b-476b1d01c936' in output[index + 2]:
+                AC = output[index + 12]
+                DC = output[index + 13]
 
-                hex_DC = DC.strip().split(':')[1].strip()
-                dec_DC = __hex_to_dec(hex_DC)
+                if 'Current AC Power Setting Index:' in AC and 'Current DC Power Setting Index:' in DC:
+                    hex_AC = AC.strip().split(':')[1].strip()
+                    dec_AC = __hex_to_dec(hex_AC)
 
-                if dec_AC == 0 and dec_DC == 0:
-                    return True
-                else:
-                    return False             
-        index += 1   
-    return None
+                    hex_DC = DC.strip().split(':')[1].strip()
+                    dec_DC = __hex_to_dec(hex_DC)
+
+                    if dec_AC == 0 and dec_DC == 0:
+                        return True
+                    else:
+                        return False             
+            index += 1   
+        return None
+
+        # TODO - implement for Windows 11
+    elif OS_VERSION == '11':
+        return CURRENT_STATE
 
 
-if (__is_State_ON()):
-    image = PIL.Image.open(resource_path('./images/ON-tray.png'))
-else:
-    image = PIL.Image.open(resource_path('./images/OFF-tray.png'))
+def on_State_Change(icon, item):
+    global CURRENT_STATE
+    
+    # Check if 10 or 11 and change accordingly
+    if OS_VERSION == '10':
+        state = __is_State_ON()
+    elif OS_VERSION == '11':
+        state = CURRENT_STATE
+        CURRENT_STATE = not CURRENT_STATE
 
+    if (state == False):
+            turn_on_commands = ['powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0', 'powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0', 'powercfg -SetActive SCHEME_CURRENT']
+            for command in turn_on_commands:
+                execute_command(command)
 
-def on_State_Change(icon, item): 
-    if (__is_State_ON() == False):
-        turn_on_commands = ['powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0', 'powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0', 'powercfg -SetActive SCHEME_CURRENT']
-        for command in turn_on_commands:
-            execute_command(command)
+            icon.icon = PIL.Image.open(resource_path('./images/ON-tray.png'))
+            print ('turned on')
 
-        icon.icon = PIL.Image.open(resource_path('./images/ON-tray.png'))
-        print ('turned on')
-
-    elif (__is_State_ON() == True):
+    elif (state == True):
+        # turn_on_commands = ['powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1', 'powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1', 'powercfg -SetActive SCHEME_CURRENT']
         turn_on_commands = ['powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1', 'powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1', 'powercfg -SetActive SCHEME_CURRENT']
         for command in turn_on_commands:
             execute_command(command)
@@ -83,6 +108,7 @@ def on_State_Change(icon, item):
 def __on_Exit(icon):
     icon.stop()
 
+
 def __icon_constructor():
     icon = pystray.Icon('LidLocker', image, 'LidLocker By Ran Nahmany', HAS_DEFAULT_ACTION = True ,menu=pystray.Menu(
     pystray.MenuItem('Change State', on_State_Change, default=True),
@@ -95,5 +121,16 @@ def __icon_constructor():
 
 
 if __name__ == "__main__":
+    OS_VERSION = __is_win10_or_11()
+    CURRENT_STATE = False
+
+    if OS_VERSION == '10':
+        if (__is_State_ON()):
+            image = PIL.Image.open(resource_path('./images/ON-tray.png'))
+        else:
+            image = PIL.Image.open(resource_path('./images/OFF-tray.png'))
+    elif OS_VERSION == '11':
+        image = PIL.Image.open(resource_path('./images/OFF-tray.png'))
+
     icon = __icon_constructor()
     icon.run()
